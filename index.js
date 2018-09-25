3b7d082c5e75ac2f78edaca740f38b0aa2d84dd1
Robert Masen <r@robertmasen.pizza>
2018-09-25 13:55:46 -0500

    listen

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ app.post('*', (req, res) => {
     console.log('client addr: ', req.headers['x-client-address']);
     console.log('body: ', req.body);
     res.send();
-})
+});
+
+app.listen(5555, err => {
+    if (err) throw err;
+    console.log('listening on 5555');
+});
