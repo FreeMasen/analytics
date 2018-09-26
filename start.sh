f61563048e75433d00310276da08adab5e5cf489
rfm <r.f.masen@gmail.com>
2018-09-26 15:44:07 +0000

    stderr

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-RUST_LOG=analytics:info,analytics:error,analytics:log ./target/release/analytics >> ~/logs/analytics.log &
+RUST_LOG=analytics:info,analytics:error,analytics:log ./target/release/analytics 2> ~/logs/analytics.log &
