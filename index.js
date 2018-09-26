579cadd04a237d24248db60b6186974e9f4e9db7
Robert Masen <r@robertmasen.pizza>
2018-09-26 10:32:02 -0500

    add test server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const ex = require('express');
+const bp = require('body-parser');
 const app = ex();
-
+app.use(bp.text());
 app.post('*', (req, res) => {
     console.log('request from ', req.originalUrl);
     console.log('client addr: ', req.headers['x-client-address']);
